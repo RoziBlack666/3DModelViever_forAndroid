e45e9acf1b321bd1b746a79ee45005b89dce5b11
Rozi <dinakaras050@gmail.com>
2026-08-27 00:27:17 +0300

    Create class ThemeData

---
+++ Assets/Scripts/Theme/ThemeData.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+[CreateAssetMenu(
+     fileName = "NewTheme" 
+    ,menuName = "3DModelViewer/Theme"
+)]
+public class ThemeData : ScriptableObject
+{
+    [Header("Основные цвета")]
+    public Color Base;
+
+    [Header("Кнопки")]
+    public Color InactiveBut;
+    public Color ActiveBut;
+
+    [Header("Маленькие кнопки")]
+    public Color InactiveBitBut;
+    public Color ActiveBitBut;
+
+    [Header("Узоры")]
+    public Color ImageInMenu;
+    public Color ImageInGallery;
+
+    [Header("Текст")]
+    public Color Text;
+
+    [Header("Галерея")]
+    public Color Gallery;
+
+    [Header("Панели")]
+    public Color PanelThatOpens;
+}
